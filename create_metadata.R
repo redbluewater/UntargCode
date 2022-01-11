5c52e27e4f508dce4654918cea08114698c9dcea
Krista Longnecker <klongnecker@whoi.edu>
2022-01-11 11:05:35 -0500

    Update README.md

--- create_metadata.R
+++ create_metadata.R
@@ -1,32 +1,32 @@
 args = commandArgs(trailingOnly=TRUE)
 suppressMessages(library(dplyr))
 
-# Read in list of all mz files and specifiy (in sbatch command) the ion Mode
-usePath <- paste0(args[1])
-ext <- ".mzML"
-pre <- paste0(usePath,"/")
-
-ionMode<-paste0(args[2])
-
-mzdatafiles <- list.files(usePath,recursive = FALSE, full.names=TRUE, pattern=glob2rx(paste0("*",ionMode,"*",ext)))
-
-# Read in list of all csv files from sequence methods and specificy ion Mode
-csvfile <- list.files(usePath, recursive=FALSE, full.name=TRUE, pattern=glob2rx(paste0("*",ionMode,"*",".csv")))
-
-# Combine all of your files into one
-all_csv<-bind_rows(lapply(csvfile,read.csv,skip=1))
-
-# This is just a blank row generated when exported that I don't need
-if("Sample.ID" %in% colnames(all_csv)){all_csv$Sample.ID<-NULL}
-
-# Filter all to keep just the ionMode and goodData of interest
-all_csv<-all_csv[which(all_csv$ionMode==ionMode & all_csv$goodData==1),]
-
-# Add a column with the full file basename
-all_csv$FileWithExtension<-paste0(all_csv$File.Name,ext)
-
 #will the code make a blank file?
 file.create("my.csv")
 
-# Check you didn't make any mistakes and then write file
-if(all(all_csv$FileWithExtension %in% basename(mzdatafiles))==TRUE){ write.table(all_csv,paste0("metadata_",ionMode,".txt"),append = FALSE, sep = "\t",row.names = FALSE,col.names=TRUE,quote=FALSE)}
+## Read in list of all mz files and specifiy (in sbatch command) the ion Mode
+#usePath <- paste0(args[1])
+#ext <- ".mzML"
+#pre <- paste0(usePath,"/")
+
+#ionMode<-paste0(args[2])
+
+#mzdatafiles <- list.files(usePath,recursive = FALSE, full.names=TRUE, pattern=glob2rx(paste0("*",ionMode,"*",ext)))
+
+## Read in list of all csv files from sequence methods and specificy ion Mode
+#csvfile <- list.files(usePath, recursive=FALSE, full.name=TRUE, pattern=glob2rx(paste0("*",ionMode,"*",".csv")))
+
+## Combine all of your files into one
+#all_csv<-bind_rows(lapply(csvfile,read.csv,skip=1))
+
+## This is just a blank row generated when exported that I don't need
+#if("Sample.ID" %in% colnames(all_csv)){all_csv$Sample.ID<-NULL}
+
+## Filter all to keep just the ionMode and goodData of interest
+#all_csv<-all_csv[which(all_csv$ionMode==ionMode & all_csv$goodData==1),]
+
+## Add a column with the full file basename
+#all_csv$FileWithExtension<-paste0(all_csv$File.Name,ext)
+
+## Check you didn't make any mistakes and then write file
+#if(all(all_csv$FileWithExtension %in% basename(mzdatafiles))==TRUE){ write.table(all_csv,paste0("metadata_",ionMode,".txt"),append = FALSE, sep = "\t",row.names = FALSE,col.names=TRUE,quote=FALSE)}

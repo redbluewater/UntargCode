d8a935741367b016ce5cd366cbb9d8bf986bd737
Krista <klongnecker@whoi.edu>
2022-01-19 11:21:28 -0500

    troubleshooting

--- scripts_dir/create_xset.R
+++ scripts_dir/create_xset.R
@@ -1,10 +1,9 @@
 args = commandArgs(trailingOnly=TRUE) #need this to use slurm(so it seems)  
 suppressMessages(library(xcms))
 
+#setwd("~/UntargCode/output_dir/xcms2")
 work_dir <- paste0(args[1])
 setwd(work_dir)
-#setwd("~/UntargCode/output_dir/xcms2")
-
 
 # polarity mode
 ionMode <- paste0(args[2])
@@ -12,26 +11,9 @@ ionMode <- paste0(args[2])
 # Load the MS OnDisk object combined in previous script
 load(file=paste0("xcms2_final-",ionMode,".RData"))
 
-## This function retrieve a xset like object and fixes the error of sample class naming, @author Gildas Le Corguille lecorguille@sb-roscoff.fr
-##? Does this help me? I think the code inserted at line 35 (from me) does the same thing
-#getxcmsSetObject <- function(xobject) {
-#    # XCMS 1.x
-#    if (class(xobject) == "xcmsSet")
-#        return (xobject)
-#    # XCMS 3.x
-#    if (class(xobject) == "XCMSnExp") {
-#        # Get the legacy xcmsSet object
-#        suppressWarnings(xset <- as(xobject, 'xcmsSet'))
-#        if (is.null(xset@phenoData$subset.name)) #KL change, Erin had xset@phenoData$sample_group ...which doesn't exist
-#            sampclass(xset) = "."
-#        else
-#            sampclass(xset) <- xset@phenoData$subset.name
-#        return (xset)
-#    }
-#}
-
 # Create the xcmsSet object
-#xset <- as(filterMsLevel(processedData, msLevel = 1L), "xcmsSet") #from KL code, need one more thing to run this in slurm
+#xset <- as(filterMsLevel(processedData, msLevel = 1L), "xcmsSet") #from KL code
+#need the tryCatch to get this to run this in slurm
 tryCatch(xset <- as(filterMsLevel(processedData, msLevel = 1L), "xcmsSet"))
 
 #I need this too (KL 1/17/2022), that's the warning I just skipped over in the previous line
@@ -42,5 +24,4 @@ xset <- fillPeaks(xset, method = "chrom")
 dim(xset@groups)
 
 # Save
-file.create(paste0(ionMode,"_testing.csv"))
 saveRDS(xset, file=paste0(ionMode,"_xset.rds"))
